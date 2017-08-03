5685cb38143d44d1db296e61800ea011b1cb6dc5
uzoahmebee <@uzoahmebee>
2017-08-03 02:41:42 +0100

    Change the background colour to white. Apply a new style definition 'blue-large' to Hello-world. Change the 'Hello World' font colour to blue and make the font size large.

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import {Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { AppRegistry, Image } from 'react-native';
+import React , {Component } from 'react';
+import { StyleSheet, Text, View , AppRegistry, Image } from 'react-native';
 
 class Blink extends Component {
   constructor(props) {
@@ -24,13 +22,13 @@ class Blink extends Component {
   }
 }
 
-export default class App extends React.Component {
+export default class App extends Component {
 
   render() {   
     return (
       <View style={styles.container}>
-         <Text>Hello world! </Text>
-         <Blink text='Blinking Text Line 1'/>
+         <Text style ={styles.blue-large}>Hello world! </Text>
+         <Blink text='Blinking Text Line 1' />
          <Blink text='Blinking Text Line 2'/>
          <Image source={require('./pictures/olusegun-samuel-seal.jpg')} style={{width: 290, height: 205}}/>
       </View>
@@ -42,8 +40,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  blue-large: {
+    color: 'blue',
+    fontSize: 30,
   },
 });
